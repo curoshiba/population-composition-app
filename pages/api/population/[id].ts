b19adf38ea52659d1a41e3dff5b7815a453dbf53
curoshiba <curoshiba.9648@gmail.com>
2022-06-10 00:41:54 +0900

    create fetchGraphData

---
+++ pages/api/population/[id].ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const Header = new Headers();
+Header.append("X-API-KEY", process.env.API_KEY as string);
+
+export default async function getPrefectures(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${req.query.id}`;
+  console.log(url);
+  const response = await fetch(url, {
+    method: "GET",
+    headers: Header,
+  });
+  const data = await response.json();
+  res.status(200).json({ data });
+}
